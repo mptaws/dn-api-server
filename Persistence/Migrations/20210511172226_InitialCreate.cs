e32aa41445764a82078bcfa02d96043b7ab06b40
Michael Tuszynski <tuszym@amazon.com>
2021-05-12 06:59:59 -0700

    test if table exists

--- Persistence/Migrations/20210511172226_InitialCreate.cs
+++ Persistence/Migrations/20210511172226_InitialCreate.cs
@@ -6,25 +6,39 @@ namespace API.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                name: "Todos",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    completed = table.Column<bool>(type: "bit", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Todos", x => x.Id);
-                });
+            if (!Exists("dbo.Todos"))
+            {
+                migrationBuilder.CreateTable(
+                    name: "Todos",
+                    columns: table => new
+                    {
+                        Id = table.Column<int>(type: "int", nullable: false)
+                            .Annotation("SqlServer:Identity", "1, 1"),
+                        Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                        completed = table.Column<bool>(type: "bit", nullable: false)
+                    },
+                    constraints: table =>
+                    {
+                        table.PrimaryKey("PK_Todos", x => x.Id);
+                    });
+            }
+
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
                 name: "Todos");
+        }
+
+        private static bool Exists(string tableName)
+        {
+            using (var context = new DbContext(config.GetValue<string>("CONNSTR"))
+            {
+                var count = context.Database.SqlQuery<int>("SELECT COUNT(OBJECT_ID(@p0, 'U'))", tableName);
+
+                return count.Any() && count.First() > 0;
+            }
         }
     }
 }
